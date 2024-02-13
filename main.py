d18b805a80fe31494cd8e8b7f84365171af11a1f
hugobaudchon <hugo@baudchon.com>
2024-02-13 12:27:04 -0500

    Modified the tilerizer to use the Raster and RasterDetectionLabels classes

--- main.py
+++ main.py
@@ -1,19 +1,13 @@
 from pathlib import Path
 
-from geodataset.geodata import Raster, PointCloud
-from geodataset.labels import DetectionLabels
-from geodataset.dataset import LabeledDetectionRasterDataset
+from geodataset.tilerize.tilerizer_new import RasterDetectionTilerizer
 
-raster = Raster(path=Path('/home/hugobaudchon/Documents/Data/raw/quebec_trees_dataset_2021-09-02/2021-09-02/zone1/2021-09-02-sbl-z1-rgb-cog.tif'),
-                scale_factor=0.5)
-labels = DetectionLabels(path=Path('/home/hugobaudchon/Documents/Data/raw/quebec_trees_dataset_2021-09-02/Z1_polygons.gpkg'),
-                         associated_geo_data=raster)
+tilerizer = RasterDetectionTilerizer(dataset_name='Z1_quebec_trees_0.5',
+                                     raster_path=Path('/home/hugobaudchon/Documents/Data/raw/quebec_trees_dataset_2021-09-02/2021-09-02/zone1/2021-09-02-sbl-z1-rgb-cog.tif'),
+                                     labels_path=Path('/home/hugobaudchon/Documents/Data/raw/quebec_trees_dataset_2021-09-02/Z1_polygons.gpkg'),
+                                     output_path=Path('/home/hugobaudchon/Documents/Data/pre_processed/test/Z1_quebec_trees'),
+                                     scale_factor=0.5)
 
-dataset = LabeledDetectionRasterDataset(data=raster,
-                                        labels=labels)
-
-print(labels.labels)
-print(raster.crs)
-print(raster.data.shape)
+tilerizer.create_tiles(tile_size=1024, overlap=0.5, start_counter_tile=0)
 
 
