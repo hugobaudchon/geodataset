f3dd92bb46afec0452abf4ba9f9e2aad8eb9ff7f
hugobaudchon <hugo@baudchon.com>
2024-02-14 12:50:07 -0500

    Added min_intersection_ratio and ignore_mostly_black_or_white_tiles parameters to the Tilerizer. Also updated the way it stores tiles by using the io module.

--- geodataset/tilerize/tilerizer.py
+++ geodataset/tilerize/tilerizer.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 from geodataset.geodata import Raster
 from geodataset.labels import RasterDetectionLabels
+from geodataset.utils.io import save_tile
 
 
 class RasterDetectionTilerizer:
@@ -23,11 +24,18 @@ class RasterDetectionTilerizer:
             path to parent folder where to save the image tiles and associated labels
         scale_factor: float
             Rescaling the data (change pixel resolution)
+        intersection_ratio: float,
+            When finding the associated labels to a tile, this ratio will specify the minimal required intersection
+            ratio between a candidate polygon and the tile in order to keep this polygon as a label for that tile.
+        ignore_mostly_black_or_white_tiles: bool,
+            Whether to ignore (skip) mostly black or white (>50%) tiles
         """
         self.dataset_name = dataset_name
         self.raster_path = raster_path
         self.labels_path = labels_path
         self.scale_factor = scale_factor
+        self.min_intersection_ratio = min_intersection_ratio
+        self.ignore_mostly_black_or_white_tiles = ignore_mostly_black_or_white_tiles
 
         self.output_path = output_path
         self.tiles_path = self.output_path / 'tiles'
@@ -40,7 +48,7 @@ class RasterDetectionTilerizer:
         raster = Raster(path=self.raster_path,
                         scale_factor=self.scale_factor)
         labels = RasterDetectionLabels(path=self.labels_path,
-                                       associated_geo_data=raster,
+                                       associated_raster=raster,
                                        scale_factor=self.scale_factor)
         return raster, labels
 
@@ -60,29 +68,36 @@ class RasterDetectionTilerizer:
             print(f'\t Row {row}/{width}')
             for col in range(0, height, int((1 - overlap) * tile_size)):
                 window = rasterio.windows.Window(col, row, tile_size, tile_size)
-                raster = self.raster.data[
+                tile = self.raster.data[
                          :,
                          window.row_off:window.row_off + window.height,
                          window.col_off:window.col_off + window.width]
 
-                # If it's >50% black pixels or white pixels, just continue. No point segmenting it.
-                if np.sum(raster == 0) / (tile_size * tile_size * self.raster.metadata['count']) > 0.5:
-                    continue
-                if np.sum(raster == 255) / (tile_size * tile_size * self.raster.metadata['count']) > 0.5:
-                    continue
+                if self.ignore_mostly_black_or_white_tiles:
+                    # If it's >50% black pixels or white pixels, just continue. No point segmenting it.
+                    if np.sum(tile == 0) / (tile_size * tile_size * self.raster.metadata['count']) > 0.5:
+                        continue
+                    if np.sum(tile == 255) / (tile_size * tile_size * self.raster.metadata['count']) > 0.5:
+                        continue
 
                 window_transform = rasterio.windows.transform(window, self.raster.metadata['transform'])
-                tile_name = 'tile_{}_{}_{}.tif'.format(self.dataset_name, row, col)
-                sample_path = self.tiles_path / tile_name
-                samples.append(sample_path)
-                with rasterio.open(
-                        sample_path, 'w',
-                        driver='GTiff',
-                        height=tile_size,
-                        width=tile_size,
-                        count=self.raster.metadata['count'],
-                        dtype=self.raster.metadata['dtype'],
-                        crs=self.raster.metadata['crs'],
-                        transform=window_transform) as tile_raster:
-                    tile_raster.write(raster)
+
+                tile_metadata = {
+                    'driver': 'GTiff',
+                    'height': tile_size,
+                    'width': tile_size,
+                    'count': self.raster.metadata['count'],
+                    'dtype': self.raster.metadata['dtype'],
+                    'crs': self.raster.metadata['crs'],
+                    'transform': window_transform
+                }
+
+                save_tile(output_folder=self.output_path,
+                          tile=tile,
+                          tile_metadata=tile_metadata,
+                          row=row,
+                          col=col,
+                          dataset_name=self.dataset_name,
+                          fold='TEST')
+
         tile_id += 1
