cd4b742f70761255a6f95fe27b3a12848569f28a
hugobaudchon <hugo@baudchon.com>
2024-02-14 12:50:52 -0500

    Added code to find associated labels to a tile

--- geodataset/tilerize/tilerizer.py
+++ geodataset/tilerize/tilerizer.py
@@ -80,6 +80,8 @@ class RasterDetectionTilerizer:
                     if np.sum(tile == 255) / (tile_size * tile_size * self.raster.metadata['count']) > 0.5:
                         continue
 
+                associated_labels = self._find_associated_labels(window=window)
+
                 window_transform = rasterio.windows.transform(window, self.raster.metadata['transform'])
 
                 tile_metadata = {
@@ -101,3 +103,45 @@ class RasterDetectionTilerizer:
                           fold='TEST')
 
         tile_id += 1
+
+    @staticmethod
+    def calculate_area(bounds):
+        """Calculate the area of a rectangle defined by bounds [minx, miny, maxx, maxy]."""
+        minx, miny, maxx, maxy = bounds
+        return (maxx - minx) * (maxy - miny)
+
+    @staticmethod
+    def _window_to_bbox(window: rasterio.windows.Window):
+        """
+        Convert a rasterio window to a bounding box [minx, miny, maxx, maxy] in pixel coordinates.
+        """
+        minx, miny = window.col_off, window.row_off
+        maxx, maxy = window.col_off + window.width, window.row_off + window.height
+        return [minx, miny, maxx, maxy]
+
+    def _find_associated_labels(self, window: rasterio.windows.Window):
+        crop_region = self._window_to_bbox(window)
+        print(crop_region)
+        intersecting_cropped_polygons = []
+        for poly in self.labels.labels:
+            # Calculate intersection
+            intersect_minx = max(poly[0], crop_region[0])
+            intersect_miny = max(poly[1], crop_region[1])
+            intersect_maxx = min(poly[2], crop_region[2])
+            intersect_maxy = min(poly[3], crop_region[3])
+
+            # Check if there is an intersection
+            if intersect_minx < intersect_maxx and intersect_miny < intersect_maxy:
+                intersect_area = self.calculate_area([intersect_minx, intersect_miny, intersect_maxx, intersect_maxy])
+                poly_area = self.calculate_area(poly)
+                ratio = intersect_area / poly_area
+
+                # Check intersection ratio
+                if ratio >= self.min_intersection_ratio:
+                    # If the polygon needs to be cropped
+                    if ratio < 1:
+                        intersecting_cropped_polygons.append([intersect_minx, intersect_miny, intersect_maxx, intersect_maxy])
+                    else:
+                        intersecting_cropped_polygons.append(poly)
+
+        return intersecting_cropped_polygons
