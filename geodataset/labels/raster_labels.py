3dd0e9c9e82f300fe569f6fd44ecd9a0b7316063
Hugo <hugo@baudchon.com>
2024-02-24 21:29:14 -0500

    Added logic to manually specify what is the main category column and what are the other attributes to keep in the COCO dataset for the labels.

--- geodataset/labels/raster_labels.py
+++ geodataset/labels/raster_labels.py
@@ -1,6 +1,6 @@
 import warnings
+from typing import List
 
-import rasterio
 import xmltodict
 import pandas as pd
 import geopandas as gpd
@@ -18,13 +18,17 @@ class RasterPolygonLabels:
                  path: Path,
                  associated_raster: Raster,
                  task: str,
-                 scale_factor: float = 1.0):
+                 scale_factor: float = 1.0,
+                 main_label_category_column_name: str = None,
+                 other_labels_attributes_column_names: List[str] = None):
 
         self.path = path
         self.ext = self.path.suffix
         self.associated_raster = associated_raster
         self.scale_factor = scale_factor
         self.task = task
+        self.main_label_category_column_name = main_label_category_column_name
+        self.other_labels_attributes_column_names = other_labels_attributes_column_names
 
         assert task in self.SUPPORTED_TASKS, \
             f"The 'task' value provided is not valid. Valid values are {self.SUPPORTED_TASKS}"
@@ -79,12 +83,30 @@ class RasterPolygonLabels:
             labels_gdf = gpd.read_file(self.path)
         else:
             raise ValueError("Unsupported file format for polygons. Please use GeoJSON (.geojson), GPKG (.gpkg) or Shapefile (.shp).")
+
+        if self.main_label_category_column_name:
+            assert self.main_label_category_column_name in labels_gdf, \
+                f'Could not find the main category {self.main_label_category_column_name}'\
+                f' in the geopackage. Please manually double check the geopackage polygons attributes'\
+                f' or change the value of parameter \'main_label_category_column_name\'.'\
+                f' The columns of the geopackages are: {labels_gdf.columns}'
+
+        if self.other_labels_attributes_column_names:
+            for attribute in self.other_labels_attributes_column_names:
+                assert self.main_label_category_column_name in labels_gdf, \
+                    f'Could not find the attribute {attribute}' \
+                    f' in the geopackage. Please manually double check the geopackage polygons attributes' \
+                    f' or remove the attribute from the parameter \'other_labels_attributes_column_names\'.' \
+                    f' The columns of the geopackages are: {labels_gdf.columns}'
+
         return labels_gdf, None#TODO
 
     def _load_xml_labels(self):
         with open(self.path, 'r') as annotation_file:
             annotation = xmltodict.parse(annotation_file.read())
         labels_bboxes = []
+        labels_main_categories = []
+        labels_other_attributes = {attribute: [] for attribute in self.other_labels_attributes_column_names}
         if isinstance(annotation['annotation']['object'], list):
             for bbox in annotation['annotation']['object']:
                 xmin = int(bbox['bndbox']['xmin'])
@@ -92,17 +114,58 @@ class RasterPolygonLabels:
                 xmax = int(bbox['bndbox']['xmax'])
                 ymax = int(bbox['bndbox']['ymax'])
                 labels_bboxes.append(box(xmin, ymin, xmax, ymax))
+                labels_main_categories, labels_other_attributes = self._find_label_attributes_in_xml(
+                    bbox_xml_object=bbox,
+                    labels_main_categories=labels_main_categories,
+                    labels_other_attributes=labels_other_attributes
+                )
         else:
-            xmin = int(annotation['annotation']['object']['bndbox']['xmin'])
-            ymin = int(annotation['annotation']['object']['bndbox']['ymin'])
-            xmax = int(annotation['annotation']['object']['bndbox']['xmax'])
-            ymax = int(annotation['annotation']['object']['bndbox']['ymax'])
+            bbox = annotation['annotation']['object']
+            xmin = int(bbox['bndbox']['xmin'])
+            ymin = int(bbox['bndbox']['ymin'])
+            xmax = int(bbox['bndbox']['xmax'])
+            ymax = int(bbox['bndbox']['ymax'])
             labels_bboxes.append(box(xmin, ymin, xmax, ymax))
+            labels_main_categories, labels_other_attributes = self._find_label_attributes_in_xml(
+                bbox_xml_object=bbox,
+                labels_main_categories=labels_main_categories,
+                labels_other_attributes=labels_other_attributes
+            )
 
         labels_gdf = gpd.GeoDataFrame(geometry=labels_bboxes)
+        # Adding main categories and other attributes in labels_gdf
+        if self.main_label_category_column_name:
+            labels_gdf['main_category'] = pd.Series(labels_main_categories)
+        if self.other_labels_attributes_column_names:
+            for attribute, values in labels_other_attributes.items():
+                labels_gdf[attribute] = pd.Series(values)
+
         is_bbox = True
 
         return labels_gdf, is_bbox
+
+    def _find_label_attributes_in_xml(self,
+                                      bbox_xml_object: dict,
+                                      labels_main_categories: list,
+                                      labels_other_attributes: dict):
+        if self.main_label_category_column_name:
+            if self.main_label_category_column_name in bbox_xml_object:
+                labels_main_categories.append(bbox_xml_object[self.main_label_category_column_name])
+            else:
+                raise Exception(f'Could not find the main category {self.main_label_category_column_name}'
+                                f' in the annotation. Please manually double check the XML labels file'
+                                f' or change the value of parameter \'main_label_category_column_name\'.')
+
+        if self.other_labels_attributes_column_names:
+            for attribute in self.other_labels_attributes_column_names:
+                if attribute in bbox_xml_object:
+                    labels_other_attributes[attribute].append(bbox_xml_object[attribute])
+                else:
+                    raise Exception(f'Could not find the attribute {attribute} in the annotation.'
+                                    f' Please manually double check the XML labels file or remove that attribute'
+                                    f' value from parameter \'other_labels_attributes_column_names\'.')
+
+        return labels_main_categories, labels_other_attributes
 
     def _load_csv_labels(self):
         labels_df = pd.read_csv(self.path)
@@ -117,34 +180,29 @@ class RasterPolygonLabels:
             labels_df = labels_df[labels_df['img_path'] == self.associated_raster.path]
             labels_bbox = labels_df[['xmin', 'ymin', 'xmax', 'ymax']].values.tolist()
 
-        if 'group' in labels_df.columns:                    # TODO add parameter to specify category columns
-            categories = labels_df['group'].to_numpy()
-        else:
-            categories = [None] * len(labels_bbox)
-
-        if 'AGB' in labels_df.columns:                      # TODO add parameter to specify category columns
-            agb = labels_df['AGB'].to_numpy()
-        else:
-            agb = None
-
         labels_gdf = gpd.GeoDataFrame(labels_df, geometry=[box(*bbox) for bbox in labels_bbox])
+        if self.main_label_category_column_name:
+            if self.main_label_category_column_name in labels_df:
+                labels_gdf[self.main_label_category_column_name] = labels_df[self.main_label_category_column_name]
+            else:
+                raise Exception(f'Could not find the main category column {self.main_label_category_column_name}'
+                                f' in the csv. Please manually double check the CSV columns'
+                                f' or change the value of parameter \'main_label_category_column_name\'.'
+                                f' The columns of the CSV are: {labels_df.columns}')
+
+        if self.other_labels_attributes_column_names:
+            for attribute in self.other_labels_attributes_column_names:
+                if attribute in labels_df:
+                    labels_gdf[attribute] = labels_df[attribute]
+                else:
+                    raise Exception(f'Could not find the attribute column {attribute} in the CSV.'
+                                    f' Please manually double check the CSV columns or remove that attribute'
+                                    f' value from parameter \'other_labels_attributes_column_names\'.'
+                                    f' The columns of the CSV are: {labels_df.columns}')
+
         is_bbox = True
 
         return labels_gdf, is_bbox
-
-    def find_associated_labels(self,
-                               window: rasterio.windows.Window,
-                               min_intersection_ratio: float):
-        intersecting_cropped_polygons = []
-        for label in self.labels:
-            # Calling label.is_bbox_in_window and get_cropped_bbox_polygon_label
-            # instead of is_in_window and get_cropped_polygon_label as we are in a detection task and only care about bbox.
-            # Those methods are much faster (about x5) than their counterparts.
-
-            if label.is_bbox_in_window(window=window, min_intersection_ratio=min_intersection_ratio):
-                intersecting_cropped_polygons.append(label.get_cropped_bbox_polygon_label(window=window))
-
-        return intersecting_cropped_polygons
 
     @staticmethod
     def try_cast_multipolygon_to_polygon(geometry):
