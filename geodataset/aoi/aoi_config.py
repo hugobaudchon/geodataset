5a5c7ba830754131d9c0a3a804523355c35f8d3d
hugobaudchon <hugo@baudchon.com>
2024-02-27 17:14:21 -0500

    Fixed a bug when creating geopackage AOI config

--- geodataset/aoi/aoi_config.py
+++ geodataset/aoi/aoi_config.py
@@ -24,7 +24,7 @@ class AOIFromPackageConfig(AOIConfig):
             assert type(aoi_name) is str, (
                 f"The keys in aois should be string, for the name of the aoi (train, valid, test...)."
                 f" Found {aoi_name} which is not a string.")
-            assert type(aoi_path) in [Path, WindowsPath], \
+            assert type(aoi_path) in [Path, PosixPath, WindowsPath], \
                 f"The value associated to aoi {aoi_name} is not a pathlib.Path. Got value {type(aoi_path)}."
 
 
