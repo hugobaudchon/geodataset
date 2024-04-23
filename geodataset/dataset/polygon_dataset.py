b4599e14f0b64bd77b513e2ee45e2944d6380448
hugobaudchon <hugo@baudchon.com>
2024-04-23 18:03:37 -0400

    Started working on a SiameseLabeledPolygonCocoDataset

--- geodataset/dataset/polygon_dataset.py
+++ geodataset/dataset/polygon_dataset.py
@@ -1,75 +1,87 @@
 from pathlib import Path
 from typing import List
 
-import numpy as np
-from shapely.affinity import translate
-from shapely.geometry import box
+import albumentations
+import rasterio
 
-from geodataset.dataset.base_dataset import BaseDataset
-from geodataset.geodata import Raster
-from geodataset.labels import RasterPolygonLabels
+from geodataset.dataset.base_dataset import BaseLabeledCocoDataset
 
 
-class FixedImageSizePolygonDataset(BaseDataset):
+class SiameseLabeledPolygonCocoDataset(BaseLabeledCocoDataset):
     def __init__(self,
-                 raster_path: Path,
-                 polygons_path: Path,
-                 image_size: int,
-                 ground_resolution: float = None,
-                 scale_factor: float = None,
-                 ):
-        self.raster_path = raster_path
-        self.polygons_path = polygons_path
-        self.image_size = image_size
-        self.ground_resolution = ground_resolution
-        self.scale_factor = scale_factor
+                 fold: str,
+                 root_path: Path or List[Path],
+                 transform: albumentations.core.composition.Compose = None):
 
-        assert not (ground_resolution and scale_factor), ("Both a ground_resolution and a scale_factor were provided."
-                                                          " Please only specify one.")
+        super().__init__(fold=fold, root_path=root_path, transform=transform)
 
-        self.raster = self._load_raster()
-        self.polygons = self._load_polygons()
+        for idx in self.tiles:
+            assert len(self.tiles[idx]['labels']) == 1, \
+                "SiameseLabeledPolygonCocoDataset dataset should have only one label per tile."
 
-    def _load_raster(self):
-        raster = Raster(path=self.raster_path,
-                        ground_resolution=self.ground_resolution,
-                        scale_factor=self.scale_factor)
-        return raster
+        self.pairs_indices = self._generate_pairs_indices()
 
-    def _load_polygons(self):
-        labels = RasterPolygonLabels(path=self.polygons_path,
-                                     associated_raster=self.raster)
-        return labels
+        # TODO add support to balance categories (instead of going through n**2/2 pairs,
+        #  go through less than that by skipping instances of the most populated classes
+        #  or choosing samples with x probability for each class)
 
-    def __getitem__(self, idx: int):
-        # Extract the polygon geometry at the given index, already in pixel coordinates
-        geom = self.polygons.geometries_gdf.iloc[idx].geometry
+        # TODO use DINOv2 Small to provide embeddings so that I can choose the best positive and negative pairs? In terms of difficulty etc.
+        # TODO =====> Or maybe instead of DINOv2, get an idea of the height/width of the objects, the area, the 'shape' (is it weird or almost a circle?) and the histogram of color distributions to choose similar/dissimilar pairs within a class or between classes (positive/negative)
+        # TODO Can also sample pairs based on if the mask is from a human or from SAM (can help the model generalize better on SAM data)
+        # TODO Can also use the area and perimeter of the mask to get an idea of the complexity of the polygon:
+        # def calculate_complexity(contour):
+        #     # Calculate area and perimeter
+        #     area = cv2.contourArea(contour)
+        #     perimeter = cv2.arcLength(contour, True)
+        #
+        #     # Circle equivalence: calculate the complexity relative to a circle
+        #     circularity = (perimeter ** 2) / (4 * np.pi * area) if area > 0 else 0
+        #
+        #     # Simplicity metric (closer to 1 is simpler, larger numbers indicate more complexity)
+        #     complexity = circularity
+        #
+        #     return complexity
+        #
+        # def find_contours(binary_image):
+        #     # Find contours in the binary image
+        #     contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        #     return contours
 
-        # Get the bounding box of the polygon
-        minx, miny, maxx, maxy = geom.bounds
 
-        # Center of the polygon
-        center_x = (minx + maxx) / 2
-        center_y = (miny + maxy) / 2
 
-        # Calculate the bounds for cropping around the center
-        half_size = self.image_size / 2
-        crop_minx = int(max(center_x - half_size, 0))
-        crop_miny = int(max(center_y - half_size, 0))
-        crop_maxx = int(min(center_x + half_size, self.raster.data.shape[2]))
-        crop_maxy = int(min(center_y + half_size, self.raster.data.shape[1]))
+    def _generate_pairs_indices(self):
+        pairs_indices = []
+        for i in range(len(self.tiles)):
+            for j in range(i + 1, len(self.tiles)):
+                pairs_indices.append((i, j))
 
-        # Crop the raster to these bounds
-        cropped_raster = self.raster.data[:, crop_miny:crop_maxy, crop_minx:crop_maxx]
-
-        # Create a new box for the cropped area and translate the geometry to be centered
-        translated_geom = translate(geom, xoff=-crop_minx, yoff=-crop_miny)
-
-        return cropped_raster, translated_geom, (crop_minx, crop_miny)
+        return pairs_indices
 
     def __len__(self):
-        return len(self.polygons.geometries_gdf)
+        return len(self.pairs_indices)
 
-    def __iter__(self):
-        for idx in range(len(self)):
-            yield self[idx]
+    def __getitem__(self, idx: int):
+        indices_pair = self.pairs_indices[idx]
+
+        tile_info_1 = self.tiles[indices_pair[0]]
+        tile_info_2 = self.tiles[indices_pair[1]]
+
+        with rasterio.open(tile_info_1['path']) as tile_file:
+            tile_1 = tile_file.read([1, 2, 3])  # Reading the first three bands
+
+        with rasterio.open(tile_info_2['path']) as tile_file:
+            tile_2 = tile_file.read([1, 2, 3])  # Reading the first three bands
+
+        category_id_1 = tile_info_1['labels'][0]['category_id']
+        category_id_2 = tile_info_2['labels'][0]['category_id']
+
+        if self.transform:
+            transformed_1 = self.transform(image=tile_1.transpose((1, 2, 0)))
+            transformed_image_1 = transformed_1['image'].transpose((2, 0, 1))
+            transformed_2 = self.transform(image=tile_2.transpose((1, 2, 0)))
+            transformed_image_2 = transformed_2['image'].transpose((2, 0, 1))
+        else:
+            transformed_image_1 = tile_1
+            transformed_image_2 = tile_2
+
+        return transformed_image_1, transformed_image_2, category_id_1, category_id_2
--- geodataset/dataset/raster_dataset.py
+++ geodataset/dataset/raster_dataset.py
@@ -1,196 +1,16 @@
-import json
 import numpy as np
-from abc import ABC, abstractmethod
 from typing import List
-from warnings import warn
 from pathlib import Path
 
 import albumentations
 import rasterio
 from shapely import box
 
-from geodataset.dataset.base_dataset import BaseDataset
-from geodataset.utils import rle_segmentation_to_bbox, polygon_segmentation_to_bbox, CocoNameConvention, \
-    rle_segmentation_to_mask
+from geodataset.dataset.base_dataset import BaseDataset, BaseLabeledRasterCocoDataset
+from geodataset.utils import rle_segmentation_to_bbox, polygon_segmentation_to_bbox, rle_segmentation_to_mask
 
 
-class LabeledRasterCocoDataset(BaseDataset, ABC):
-    def __init__(self,
-                 fold: str,
-                 root_path: Path or List[Path],
-                 transform: albumentations.core.composition.Compose = None):
-        """
-        Parameters:
-        - fold: str, the dataset fold to load (e.g., 'train', 'valid', 'test'...).
-        - root_path: pathlib.Path, the root directory of the dataset.
-        - transform: albumentations.core.composition.Compose, a composition of transformations to apply to the tiles.
-        """
-        self.fold = fold
-        self.root_path = root_path
-        self.transform = transform
-        self.tiles = {}
-        self.tiles_path_to_id_mapping = {}
-        self.cocos_detected = []
-
-        if isinstance(self.root_path, Path):
-            self.root_path = [self.root_path]
-
-        self._load_coco_datasets(directories=self.root_path)
-        self._find_tiles_paths(directories=self.root_path)
-        self._remove_tiles_not_found()
-        self._filter_tiles_without_box()
-
-        if len(self.cocos_detected) == 0:
-            raise Exception(f"No COCO datasets for fold '{self.fold}' were found in the specified root folder.")
-        elif len(self.cocos_detected) > 0 and len(self) == 0:
-            raise Exception(f"Could not find find any tiles associated with the COCO files found."
-                            f" COCO files found: {self.cocos_detected}.")
-
-        print(f"Found {len(self.tiles)} tiles and {sum([len(self.tiles[x]['labels']) for x in self.tiles])} labels"
-              f" for fold {self.fold}.")
-
-    def _load_coco_datasets(self, directories: List[Path]):
-        """
-        Loads the dataset by traversing the directory tree and loading relevant COCO JSON files.
-        """
-        for directory in directories:
-            for path in directory.iterdir():
-                if path.is_file() and path.name.endswith(f".json"):
-                    try:
-                        product_name, scale_factor, ground_resolution, fold = CocoNameConvention.parse_name(path.name)
-                        if fold == self.fold:
-                            self._load_coco_json(json_path=path)
-                    except ValueError:
-                        return
-                elif path.is_dir() and path.name != 'tiles':
-                    self._load_coco_datasets(directories=[path])
-
-    def _load_coco_json(self, json_path: Path):
-        """
-        Loads a COCO JSON file and its associated tiles.
-
-        Parameters:
-        - json_path: pathlib.Path, the path to the COCO JSON file.
-        """
-
-        with open(json_path) as f:
-            coco_data = json.load(f)
-        self._reindex_coco_data(coco_data=coco_data)
-        self.cocos_detected.append(json_path)
-
-    def _reindex_coco_data(self, coco_data: dict):
-        """
-        Re-indexes the COCO data to ensure unique tile and annotation IDs.
-
-        Parameters:
-        - coco_data: dict, the COCO dataset loaded from a JSON file.
-        """
-        coco_tiles = {}
-        for image in coco_data['images']:
-            tile_name = image['file_name']
-
-            if tile_name not in self.tiles_path_to_id_mapping:
-                new_tile_id = len(self.tiles)
-                self.tiles_path_to_id_mapping[tile_name] = new_tile_id
-                self.tiles[new_tile_id] = {
-                    'name': tile_name,
-                    'labels': []
-                }
-
-            # Keeping track of the original tile id from the COCO dataset to the tile name
-            coco_tiles[image['id']] = tile_name
-
-        for annotation in coco_data['annotations']:
-            # Finding the associated tile name given the tile id from the COCO dataset
-            tile_name = coco_tiles[annotation['image_id']]
-            # Finding the tile id in this python dataset, not the tile id in the original COCO dataset
-            tile_id = self.tiles_path_to_id_mapping[tile_name]
-            self.tiles[tile_id]['labels'].append(annotation)
-
-    def _find_tiles_paths(self, directories: List[Path]):
-        """
-        Loads the dataset by traversing the directory tree and loading relevant COCO JSON files.
-        """
-        for directory in directories:
-            if directory.is_dir() and directory.name == 'tiles':
-                for tile_path in directory.iterdir():
-                    if tile_path.name in self.tiles_path_to_id_mapping:
-                        tile_id = self.tiles_path_to_id_mapping[tile_path.name]
-                        if 'path' in self.tiles[tile_id] and self.tiles[tile_id]['path'] != tile_path:
-                            raise Exception(
-                                f"At least two tiles under the root directories {self.root_path} have the same"
-                                f" name, which is ambiguous. Make sure all tiles have unique names. The 2"
-                                f" ambiguous tiles are {tile_path} and {self.tiles[tile_id]['path']}.")
-                        self.tiles[tile_id]['path'] = tile_path
-
-            if directory.is_dir():
-                for path in directory.iterdir():
-                    if path.is_dir():
-                        self._find_tiles_paths(directories=[path])
-
-    def _remove_tiles_not_found(self):
-        original_tiles_number = len(self.tiles)
-        tile_ids_to_delete = []
-        for tile_id, tile in self.tiles.items():
-            if 'path' not in tile:
-                tile_ids_to_delete.append(tile_id)
-
-        for tile_id in tile_ids_to_delete:
-            del self.tiles[tile_id]
-
-        new_tiles_number = len(self.tiles)
-
-        if original_tiles_number != new_tiles_number:
-            warn(f"Had to remove {original_tiles_number - new_tiles_number} tiles out of {original_tiles_number}"
-                 f" as they could not be found in the root folder or its sub-folders.")
-            # Need to reindex the tile ids for __getitem__
-            self._reindex_tiles()
-
-    def _filter_tiles_without_box(self):
-        # Remove tiles without annotation for training
-        original_tiles_number = len(self.tiles)
-        if self.fold == 'train':
-            tile_ids_without_labels = []
-            for tile_id, tile in self.tiles.items():
-                if len(tile['labels']) == 0:
-                    tile_ids_without_labels.append(tile_id)
-
-            for tile_id in tile_ids_without_labels:
-                del self.tiles[tile_id]
-
-        new_tiles_number = len(self.tiles)
-
-        if original_tiles_number != new_tiles_number:
-            warn(f"Had to remove {original_tiles_number - new_tiles_number} tiles out of {original_tiles_number}"
-                 f" as they do not contain annotation in the training set.")
-            # Need to reindex the tile ids for __getitem__
-            self._reindex_tiles()
-
-    def _reindex_tiles(self):
-        reindexed_tiles = {}
-        for i, (_, tile) in enumerate(self.tiles.items()):
-            reindexed_tiles[i] = tile
-        self.tiles = reindexed_tiles
-
-    @abstractmethod
-    def __getitem__(self, idx: int):
-        pass
-
-    def __len__(self):
-        """
-        Returns the total number of tiles in the dataset.
-
-        Returns:
-        - An integer count of the tiles.
-        """
-        return len(self.tiles)
-
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self[i]
-
-
-class DetectionLabeledRasterCocoDataset(LabeledRasterCocoDataset):
+class DetectionLabeledRasterCocoDataset(BaseLabeledRasterCocoDataset):
     def __init__(self, fold: str, root_path: Path or List[Path], transform: albumentations.core.composition.Compose = None):
         super().__init__(fold=fold, root_path=root_path, transform=transform)
 
@@ -258,7 +78,7 @@ class DetectionLabeledRasterCocoDataset(LabeledRasterCocoDataset):
         return transformed_image, transformed_bboxes
 
 
-class SegmentationLabeledRasterCocoDataset(LabeledRasterCocoDataset):
+class SegmentationLabeledRasterCocoDataset(BaseLabeledRasterCocoDataset):
     def __init__(self, fold: str, root_path: Path or List[Path], transform: albumentations.core.composition.Compose = None):
         super().__init__(fold=fold, root_path=root_path, transform=transform)
 
