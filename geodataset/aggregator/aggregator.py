c23a75ff31bda29cd76ec425c27ba12f8f6f6380
Hugo <hugo@baudchon.com>
2024-04-28 08:51:25 -0400

    Refactored the DetectionAggregator into Aggregator, which now supports boxes AND masks

--- geodataset/aggregator/aggregator.py
+++ geodataset/aggregator/aggregator.py
@@ -10,23 +10,26 @@ from shapely import box, Polygon
 import geopandas as gpd
 from tqdm import tqdm
 
-from geodataset.utils import TileNameConvention, apply_affine_transform, COCOGenerator
+from geodataset.utils import TileNameConvention, apply_affine_transform, COCOGenerator, decode_rle_to_polygon
 
 
-class DetectionAggregator:
+class Aggregator:
+    SUPPORTED_POLYGON_TYPES = ['box', 'segmentation']
     SUPPORTED_NMS_ALGORITHMS = ['iou', 'diou']
 
     def __init__(self,
+                 polygon_type: str,
                  output_path: Path,
-                 boxes_gdf: gpd.GeoDataFrame,
+                 polygons_gdf: gpd.GeoDataFrame,
                  tiles_extent_gdf: gpd.GeoDataFrame,
                  tile_ids_to_path: dict or None,
                  score_threshold: float = 0.1,
                  nms_threshold: float = 0.8,
                  nms_algorithm: str = 'iou'):
 
+        self.polygon_type = polygon_type
         self.output_path = output_path
-        self.boxes_gdf = boxes_gdf
+        self.polygons_gdf = polygons_gdf
         self.tiles_extent_gdf = tiles_extent_gdf
         self.tile_ids_to_path = tile_ids_to_path
         self.score_threshold = score_threshold
@@ -34,13 +37,15 @@ class DetectionAggregator:
         self.nms_algorithm = nms_algorithm
 
         self._check_parameters()
-        self.remove_low_score_boxes()
+        self.remove_low_score_polygons()
         self.apply_nms_algorithm()
-        self.save_boxes()
+        self.save_polygons()
 
     def _check_parameters(self):
-        assert self.boxes_gdf.crs, "The provided boxes_gdf doesn't have a CRS."
-        assert 'tile_id' in self.boxes_gdf, "The provided boxes_gdf doesn't have a 'tile_id' column."
+        assert self.polygon_type in self.SUPPORTED_POLYGON_TYPES, \
+            f"The polygon_type must be one of {self.SUPPORTED_POLYGON_TYPES}. Got {self.polygon_type}."
+        assert self.polygons_gdf.crs, "The provided polygons_gdf doesn't have a CRS."
+        assert 'tile_id' in self.polygons_gdf, "The provided polygons_gdf doesn't have a 'tile_id' column."
         assert self.tiles_extent_gdf.crs, "The provided tiles_extent_gdf doesn't have a CRS."
         assert 'tile_id' in self.tiles_extent_gdf, "The provided tiles_extent_gdf doesn't have a 'tile_id' column."
         assert self.nms_algorithm in self.SUPPORTED_NMS_ALGORITHMS, \
@@ -51,10 +56,11 @@ class DetectionAggregator:
 
         if self.output_path.suffix == '.json':
             assert self.tile_ids_to_path is not None, \
-                "The tile_ids_to_path must be provided to save the boxes in COCO format."
+                "The tile_ids_to_path must be provided to save the polygons in COCO format."
 
     @classmethod
     def from_coco(cls,
+                  polygon_type: str,
                   output_path: Path,
                   tiles_folder_path: Path,
                   coco_json_path: Path,
@@ -66,17 +72,20 @@ class DetectionAggregator:
         with open(coco_json_path, 'r') as f:
             coco_data = json.load(f)
 
-        # Reading the boxes
-        tile_ids_to_boxes = {}
+        # Reading the polygons
+        tile_ids_to_polygons = {}
         tile_ids_to_scores = {}
         for annotation in coco_data['annotations']:
             image_id = annotation['image_id']
 
-            xmin = annotation['bbox'][0]
-            xmax = annotation['bbox'][0] + annotation['bbox'][2]
-            ymin = annotation['bbox'][1]
-            ymax = annotation['bbox'][1] + annotation['bbox'][3]
-            annotation_box = box(xmin, ymin, xmax, ymax)
+            if polygon_type == 'box':
+                xmin = annotation['bbox'][0]
+                xmax = annotation['bbox'][0] + annotation['bbox'][2]
+                ymin = annotation['bbox'][1]
+                ymax = annotation['bbox'][1] + annotation['bbox'][3]
+                annotation_polygon = box(xmin, ymin, xmax, ymax)
+            else:
+                annotation_polygon = decode_rle_to_polygon(annotation['segmentation'])
 
             if "score" in annotation:
                 score = annotation["score"]
@@ -87,11 +96,11 @@ class DetectionAggregator:
             else:
                 score = None
 
-            if image_id in tile_ids_to_boxes:
-                tile_ids_to_boxes[image_id].append(annotation_box)
+            if image_id in tile_ids_to_polygons:
+                tile_ids_to_polygons[image_id].append(annotation_polygon)
                 tile_ids_to_scores[image_id].append(score)
             else:
-                tile_ids_to_boxes[image_id] = [annotation_box]
+                tile_ids_to_polygons[image_id] = [annotation_polygon]
                 tile_ids_to_scores[image_id] = [score]
 
         tile_ids_to_path = {}
@@ -101,12 +110,14 @@ class DetectionAggregator:
                                          f" in tiles_folder_path='{tiles_folder_path}'")
             tile_ids_to_path[image['id']] = image_path
 
-        all_boxes_gdf, all_tiles_extents_gdf = DetectionAggregator.prepare_boxes(tile_ids_to_path=tile_ids_to_path,
-                                                                                 tile_ids_to_boxes=tile_ids_to_boxes,
-                                                                                 tile_ids_to_scores=tile_ids_to_scores)
+        all_polygons_gdf, all_tiles_extents_gdf = Aggregator.prepare_polygons(
+            tile_ids_to_path=tile_ids_to_path,
+            tile_ids_to_polygons=tile_ids_to_polygons,
+            tile_ids_to_scores=tile_ids_to_scores)
 
-        return cls(output_path=output_path,
-                   boxes_gdf=all_boxes_gdf,
+        return cls(polygon_type=polygon_type,
+                   output_path=output_path,
+                   polygons_gdf=all_polygons_gdf,
                    tiles_extent_gdf=all_tiles_extents_gdf,
                    score_threshold=score_threshold,
                    nms_threshold=nms_threshold,
@@ -114,29 +125,32 @@ class DetectionAggregator:
                    tile_ids_to_path=tile_ids_to_path)
 
     @classmethod
-    def from_boxes(cls,
-                   output_path: Path,
-                   tiles_paths: List[Path],
-                   boxes: List[List[box]],
-                   scores: List[List[float]],
-                   score_threshold: float = 0.1,
-                   nms_threshold: float = 0.8,
-                   nms_algorithm: str = 'iou'):
+    def from_polygons(cls,
+                      polygon_type: str,
+                      output_path: Path,
+                      tiles_paths: List[Path],
+                      polygons: List[List[Polygon]],
+                      scores: List[List[float]],
+                      score_threshold: float = 0.1,
+                      nms_threshold: float = 0.8,
+                      nms_algorithm: str = 'iou'):
 
-        assert len(tiles_paths) == len(boxes), ("The number of tiles_paths must be equal than the number of lists "
-                                                "in boxes (one list of boxes per tile).")
+        assert len(tiles_paths) == len(polygons), ("The number of tiles_paths must be equal than the number of lists "
+                                                   "in polygons (one list of polygons per tile).")
 
         ids = list(range(len(tiles_paths)))
         tile_ids_to_path = {k: v for k, v in zip(ids, tiles_paths)}
-        tile_ids_to_boxes = {k: v for k, v in zip(ids, boxes)}
+        tile_ids_to_polygons = {k: v for k, v in zip(ids, polygons)}
         tile_ids_to_scores = {k: v for k, v in zip(ids, scores)}
 
-        all_boxes_gdf, all_tiles_extents_gdf = DetectionAggregator.prepare_boxes(tile_ids_to_path=tile_ids_to_path,
-                                                                                 tile_ids_to_boxes=tile_ids_to_boxes,
-                                                                                 tile_ids_to_scores=tile_ids_to_scores)
+        all_polygons_gdf, all_tiles_extents_gdf = Aggregator.prepare_polygons(
+            tile_ids_to_path=tile_ids_to_path,
+            tile_ids_to_polygons=tile_ids_to_polygons,
+            tile_ids_to_scores=tile_ids_to_scores)
 
-        return cls(output_path=output_path,
-                   boxes_gdf=all_boxes_gdf,
+        return cls(polygon_type=polygon_type,
+                   output_path=output_path,
+                   polygons_gdf=all_polygons_gdf,
                    tiles_extent_gdf=all_tiles_extents_gdf,
                    score_threshold=score_threshold,
                    nms_threshold=nms_threshold,
@@ -144,11 +158,11 @@ class DetectionAggregator:
                    tile_ids_to_path=tile_ids_to_path)
 
     @staticmethod
-    def prepare_boxes(tile_ids_to_path: dict, tile_ids_to_boxes: dict, tile_ids_to_scores: dict):
+    def prepare_polygons(tile_ids_to_path: dict, tile_ids_to_polygons: dict, tile_ids_to_scores: dict):
         images_without_crs = 0
-        all_gdfs_boxes = []
+        all_gdfs_polygons = []
         all_gdfs_tiles_extents = []
-        for tile_id in tile_ids_to_boxes.keys():
+        for tile_id in tile_ids_to_polygons.keys():
             image_path = tile_ids_to_path[tile_id]
 
             src = rasterio.open(image_path)
@@ -160,14 +174,14 @@ class DetectionAggregator:
             # Making sure the Tile respects the convention
             TileNameConvention.parse_name(Path(image_path).name)
 
-            gdf_boxes = gpd.GeoDataFrame(geometry=tile_ids_to_boxes[tile_id])
-            gdf_boxes['geometry'] = gdf_boxes['geometry'].astype(object).apply(
+            gdf_polygons = gpd.GeoDataFrame(geometry=tile_ids_to_polygons[tile_id])
+            gdf_polygons['geometry'] = gdf_polygons['geometry'].astype(object).apply(
                 lambda geom: apply_affine_transform(geom, src.transform)
             )
-            gdf_boxes.crs = src.crs
-            gdf_boxes['tile_id'] = tile_id
-            gdf_boxes['score'] = tile_ids_to_scores[tile_id]
-            all_gdfs_boxes.append(gdf_boxes)
+            gdf_polygons.crs = src.crs
+            gdf_polygons['tile_id'] = tile_id
+            gdf_polygons['score'] = tile_ids_to_scores[tile_id]
+            all_gdfs_polygons.append(gdf_polygons)
 
             bounds = src.bounds
             # Create a polygon from the bounds
@@ -188,29 +202,30 @@ class DetectionAggregator:
         n_crs = len(set(tiles_crs))
         if n_crs > 1:
             common_crs = tiles_crs[0].crs
-            warn(f"Multiple CRS were detected (n={n_crs}), so re-projecting all boxes of all tiles to the"
+            warn(f"Multiple CRS were detected (n={n_crs}), so re-projecting all polygons of all tiles to the"
                  f" same common CRS '{common_crs}', chosen from one of the tiles.")
-            all_gdfs_boxes = [gdf.to_crs(common_crs) for gdf in all_gdfs_boxes if gdf.crs != common_crs]
+            all_gdfs_polygons = [gdf.to_crs(common_crs) for gdf in all_gdfs_polygons if gdf.crs != common_crs]
             all_gdfs_tiles_extents = [gdf.to_crs(common_crs) for gdf in all_gdfs_tiles_extents if gdf.crs != common_crs]
 
-        all_boxes_gdf = gpd.GeoDataFrame(pd.concat(all_gdfs_boxes, ignore_index=True))
+        all_polygons_gdf = gpd.GeoDataFrame(pd.concat(all_gdfs_polygons, ignore_index=True))
         all_tiles_extents_gdf = gpd.GeoDataFrame(pd.concat(all_gdfs_tiles_extents, ignore_index=True))
 
-        return all_boxes_gdf, all_tiles_extents_gdf
+        return all_polygons_gdf, all_tiles_extents_gdf
 
-    def remove_low_score_boxes(self):
+    def remove_low_score_polygons(self):
         if self.score_threshold:
-            if "score" in self.boxes_gdf and self.boxes_gdf["score"].any():
-                n_before = len(self.boxes_gdf)
-                self.boxes_gdf.drop(self.boxes_gdf[self.boxes_gdf["score"] < self.score_threshold].index, inplace=True)
-                n_after = len(self.boxes_gdf)
-                print(f"Removed {n_before-n_after} out of {n_before} boxes"
+            if "score" in self.polygons_gdf and self.polygons_gdf["score"].any():
+                n_before = len(self.polygons_gdf)
+                self.polygons_gdf.drop(self.polygons_gdf[self.polygons_gdf["score"] < self.score_threshold].index,
+                                       inplace=True)
+                n_after = len(self.polygons_gdf)
+                print(f"Removed {n_before - n_after} out of {n_before} polygons"
                       f" as they were under the score_threshold={self.score_threshold}.")
             else:
                 warn(f"Could not apply score_threshold={self.score_threshold} as scores were not found"
-                     f" in the boxes_gdf. If you instanced the Aggregator from a COCO file,"
+                     f" in the polygons_gdf. If you instanced the Aggregator from a COCO file,"
                      f" please make sure that ALL annotations have a 'score' key or a ['other_attributes']['score']"
-                     f" nested keys. If you instanced the Aggregator from a list of boxes for each tile,"
+                     f" nested keys. If you instanced the Aggregator from a list of polygons for each tile,"
                      f" make sure you also pass the associated list of scores for each tile.")
 
     @staticmethod
@@ -261,20 +276,20 @@ class DetectionAggregator:
         return distances
 
     def apply_nms_algorithm(self):
-        n_before = len(self.boxes_gdf)
+        n_before = len(self.polygons_gdf)
         if self.nms_algorithm == "iou":
             self.apply_iou_nms_algorithm()
         if self.nms_algorithm == "diou":
             self.apply_diou_nms_algorithm()
 
-                                                            # TODO add support for adapative-nms: https://arxiv.org/pdf/1904.03629.pdf
+            # TODO add support for adapative-nms: https://arxiv.org/pdf/1904.03629.pdf
 
-        n_after = len(self.boxes_gdf)
-        print(f"Removed {n_before - n_after} out of {n_before} boxes"
+        n_after = len(self.polygons_gdf)
+        print(f"Removed {n_before - n_after} out of {n_before} polygons"
               f" by applying the Non-Maximum Suppression-{self.nms_algorithm} algorithm.")
 
     def apply_iou_nms_algorithm(self):
-        gdf = self.boxes_gdf.copy()
+        gdf = self.polygons_gdf.copy()
         gdf.sort_values(by='score', ascending=False, inplace=True)
 
         intersect_gdf = gpd.sjoin(gdf, gdf, how='inner', predicate='intersects')
@@ -302,10 +317,10 @@ class DetectionAggregator:
 
         progress.close()
         drop_ids = list(skip_ids - keep_ids)
-        self.boxes_gdf.drop(drop_ids, inplace=True, errors="ignore")
+        self.polygons_gdf.drop(drop_ids, inplace=True, errors="ignore")
 
     def apply_diou_nms_algorithm(self):
-        gdf = self.boxes_gdf.copy()
+        gdf = self.polygons_gdf.copy()
         gdf.sort_values(by='score', ascending=False, inplace=True)
 
         gdf["centroid"] = gdf.geometry.centroid
@@ -339,36 +354,39 @@ class DetectionAggregator:
 
         progress.close()
         drop_ids = list(skip_ids - keep_ids)
-        self.boxes_gdf.drop(drop_ids, inplace=True, errors="ignore")
+        self.polygons_gdf.drop(drop_ids, inplace=True, errors="ignore")
 
-    def save_boxes(self):
+    def save_polygons(self):
         self.output_path.parent.mkdir(parents=True, exist_ok=True)
 
         if self.output_path.suffix == '.geojson':
-            self.boxes_gdf.to_file(str(self.output_path), driver="GeoJSON")
+            self.polygons_gdf.to_file(str(self.output_path), driver="GeoJSON")
         elif self.output_path.suffix == '.json':
-            tiles_ids = self.boxes_gdf['tile_id'].unique()
+            tiles_ids = self.polygons_gdf['tile_id'].unique()
             coco_generator = COCOGenerator(
-                description=f"Aggregated boxes from multiple tiles.",
+                description=f"Aggregated polygons from multiple tiles.",
                 tiles_paths=[self.tile_ids_to_path[tile_id] for tile_id in tiles_ids],
-                polygons=[self._apply_inverse_transform(boxes=self.boxes_gdf[self.boxes_gdf['tile_id'] == tile_id]['geometry'].tolist(),
-                                                        tile_path=self.tile_ids_to_path[tile_id]) for tile_id in tiles_ids],
-                scores=[self.boxes_gdf[self.boxes_gdf['tile_id'] == tile_id]['score'].tolist() for tile_id in tiles_ids],
-                categories=None,                        # TODO add support for categories
-                other_attributes=None,                  # TODO add support for other_attributes
+                polygons=[self._apply_inverse_transform(
+                    polygons=self.polygons_gdf[self.polygons_gdf['tile_id'] == tile_id]['geometry'].tolist(),
+                    tile_path=self.tile_ids_to_path[tile_id]) for tile_id in tiles_ids],
+                scores=[self.polygons_gdf[self.polygons_gdf['tile_id'] == tile_id]['score'].tolist() for tile_id in
+                        tiles_ids],
+                categories=None,  # TODO add support for categories
+                other_attributes=None,  # TODO add support for other_attributes
                 output_path=self.output_path,
-                use_rle_for_labels=True,                # TODO make this a parameter to the class
-                n_workers=5,                            # TODO make this a parameter to the class
-                coco_categories_list=None      # TODO make this a parameter to the class
+                use_rle_for_labels=True,  # TODO make this a parameter to the class
+                n_workers=5,  # TODO make this a parameter to the class
+                coco_categories_list=None  # TODO make this a parameter to the class
             )
             coco_generator.generate_coco()
         else:
-            raise Exception("The output_path needs to end with either .json (coco output) or .geojson (geopackage output).")
-        print(f"Saved {len(self.boxes_gdf)} boxes in file '{self.output_path}'.")
+            raise Exception(
+                "The output_path needs to end with either .json (coco output) or .geojson (geopackage output).")
+        print(f"Saved {len(self.polygons_gdf)} polygons in file '{self.output_path}'.")
 
     @staticmethod
-    def _apply_inverse_transform(boxes: List[Polygon], tile_path: Path):
+    def _apply_inverse_transform(polygons: List[Polygon], tile_path: Path):
         src = rasterio.open(tile_path)
         inverse_transform = ~src.transform
-        boxes = [apply_affine_transform(box, inverse_transform) for box in boxes]
-        return boxes
+        polygons = [apply_affine_transform(polygon, inverse_transform) for polygon in polygons]
+        return polygons
