eb1f2ae3c25d528829051eb53bc126bc47aa6a22
hugobaudchon <hugo@baudchon.com>
2024-06-05 12:09:13 -0400

    Added GeoPackage (.gpkg) name convention

--- geodataset/utils/__init__.py
+++ geodataset/utils/__init__.py
@@ -3,5 +3,6 @@ from .file_name_conventions import (validate_and_convert_product_name,
                                     TileNameConvention,
                                     PolygonTileNameConvention,
                                     GeoJsonNameConvention,
+                                    GeoPackageNameConvention,
                                     CocoNameConvention,
                                     AoiTilesImageConvention)
