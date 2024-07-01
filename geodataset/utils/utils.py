4c6112d579ba4aa7c2db5467facea5e922ed5cec
Hugo <hugo@baudchon.com>
2024-07-01 01:11:09 -0400

    added mask_to_polygon utils function

--- geodataset/utils/utils.py
+++ geodataset/utils/utils.py
@@ -21,6 +21,7 @@ import geopandas as gpd
 from rasterio.enums import Resampling
 from shapely import Polygon, box, MultiPolygon
 from shapely.ops import transform
+from skimage.measure import find_contours
 
 
 def polygon_to_coco_coordinates(polygon: Polygon or MultiPolygon):
@@ -103,6 +104,49 @@ def rle_segmentation_to_bbox(segmentation: dict) -> box:
     xmin, xmax = np.where(cols)[0][[0, -1]]
 
     return box(xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax)
+
+
+def mask_to_polygon(mask: np.ndarray, simplify_tolerance: float = 1.0) -> Polygon:
+    """
+    Converts a 1HW mask to a simplified shapely Polygon by finding the contours of the mask
+    and simplifying it.
+
+    Parameters:
+    - mask (np.ndarray): The mask to convert, in 1HW format.
+    - simplify_tolerance (float): The tolerance for simplifying the polygon. Higher values
+      result in more simplified shapes.
+
+    Returns:
+    - Polygon: A simplified shapely Polygon object representing the outer boundary of the mask.
+    """
+    # Ensure mask is 2D
+    if mask.ndim != 2:
+        raise ValueError("Mask must be in HW format (2D array).")
+
+    # Pad the mask to avoid boundary issues
+    padded_mask = np.pad(mask, pad_width=1, mode='constant', constant_values=0)
+
+    # Find contours on the mask, assuming mask is binary
+    contours = find_contours(padded_mask, 0.5)
+
+    if len(contours) == 0:
+        # returning empty, dummy polygon at 0,0
+        return Polygon([(0, 0), (0, 0), (0, 0)])
+
+    # Take the longest contour as the main outline of the object
+    longest_contour = max(contours, key=len)
+
+    # Convert contour coordinates from (row, column) to (x, y)
+    # and revert the padding added to the mask
+    longest_contour_adjusted_xy = [(y - 1, x - 1) for x, y in longest_contour]
+
+    # Convert contour to Polygon
+    polygon = Polygon(longest_contour_adjusted_xy)
+
+    # Simplify the polygon
+    simplified_polygon = polygon.simplify(tolerance=simplify_tolerance, preserve_topology=True)
+
+    return simplified_polygon
 
 
 def polygon_segmentation_to_polygon(segmentation: list) -> Polygon:
