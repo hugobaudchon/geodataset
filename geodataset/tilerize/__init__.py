ac7d0f13d7418e5a029be3d7c2768de45b61c5ca
jannikend <jannikendres@mail.de>
2025-08-04 06:46:25 -0400

    first version of point cloud polygon tilerizer

--- geodataset/tilerize/__init__.py
+++ geodataset/tilerize/__init__.py
@@ -2,6 +2,8 @@ from .raster_tilerizer import RasterTilerizer, RasterTilerizerGDF
 from .labeled_raster_tilerizer import LabeledRasterTilerizer
 from .raster_polygon_tilerizer import RasterPolygonTilerizer
 
+from .point_cloud_polygon_tilerizer import PointCloudPolygonTilerizer
+
 try:
     from .point_cloud_tilerizer import PointCloudTilerizer
     from .labeled_point_cloud_tilerizer import LabeledPointCloudTilerizer
