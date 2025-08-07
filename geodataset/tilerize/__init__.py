1d99e9ec7075ea031d0d6f7ce92010674dadca1a
jannikend <jannikendres@mail.de>
2025-08-07 05:21:44 -0400

    code of point cloud polygon tilerizer improved

--- geodataset/tilerize/__init__.py
+++ geodataset/tilerize/__init__.py
@@ -2,10 +2,9 @@ from .raster_tilerizer import RasterTilerizer, RasterTilerizerGDF
 from .labeled_raster_tilerizer import LabeledRasterTilerizer
 from .raster_polygon_tilerizer import RasterPolygonTilerizer
 
-from .point_cloud_polygon_tilerizer import PointCloudPolygonTilerizer
-
 try:
     from .point_cloud_tilerizer import PointCloudTilerizer
     from .labeled_point_cloud_tilerizer import LabeledPointCloudTilerizer
+    from .point_cloud_polygon_tilerizer import PointCloudPolygonTilerizer
 except ImportError:
     pass
